93e58631ecb1d0ab236120880bb6914763f98e45
wzz <ywc0008@naver.com>
2024-02-20 22:10:52 +0900

    플러스 마이너스 곱 나누기 추가

--- toDoList/src/App.tsx
+++ toDoList/src/App.tsx
@@ -2,14 +2,26 @@ import { useState } from "react";
 
 function App() {
   const [counter, setCounter] = useState(0);
-  const onClick = () => {
+  const onPlusClick = () => {
     setCounter((current) => current + 1);
+  };
+  const onMinusClick = () => {
+    setCounter((current) => current - 1);
+  };
+  const onMultipleClick = () => {
+    setCounter((current) => current * 2);
+  };
+  const onDivideClick = () => {
+    setCounter((current) => current / 2);
   };
   return (
     <div>
       <h1>React Counter</h1>
       <h3>{counter}</h3>
-      <button onClick={onClick}>클릭</button>
+      <button onClick={onPlusClick}>+클릭</button>
+      <button onClick={onMinusClick}>-클릭</button>
+      <button onClick={onMultipleClick}>X2 클릭</button>
+      <button onClick={onDivideClick}>/2 클릭</button>
     </div>
   );
 }
